3cf9c1fcc2aa25dc87acfcf091c8f4c214ddd816
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-04-30 17:18:52 +0300

    add start script

---
+++ init.el
@@ -0,0 +1,28 @@
+(add-to-list 'load-path "~/emacs/external")
+
+(load "~/emacs/rc/core-packages.el")
+
+(load "~/emacs/rc/core-ui.el")
+(load "~/emacs/rc/core-lang.el")
+(load "~/emacs/rc/core-desktop.el")
+
+(when (display-graphic-p)
+ (load "~/emacs/rc/core-fonts.el"))
+
+(load "~/emacs/rc/autocomplete.el")
+(load "~/emacs/rc/helm.el")
+
+(load "~/emacs/rc/assoc.el")
+(load "~/emacs/rc/cmode.el")
+(load "~/emacs/rc/coffee.el")
+(load "~/emacs/rc/smartparens.el")
+
+(load "~/emacs/rc/minimap.el")
+(when (display-graphic-p)
+  (load "~/emacs/rc/mingus.el"))
+
+(load "~/emacs/rc/js.el")
+(load "~/emacs/rc/web.el")
+
+(load "~/emacs/rc/jabber.el")
+(load "~/emacs/rc/perlgods.el")
