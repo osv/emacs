8484fc93acbed32a993d8b363f160ab7338fbb72
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-05-04 13:03:59 +0300

    company customize

--- rc/company-completion.el
+++ rc/company-completion.el
@@ -1,4 +1,15 @@
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 
-(define-key ac-completing-map (kbd "C-g") 'company-files)
+(global-set-key (kbd "C-c /") 'company-files)
+
+;;(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-tooltip-align-annotations 't)          ; align annotations to the right tooltip border
+(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+
+(eval-after-load 'company
+  '(progn
+     (define-key company-mode-map (kbd "C-'") 'helm-company)
+     (define-key company-active-map (kbd "C-'") 'helm-company)))
+
