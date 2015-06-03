b99a9e7ba6e4fae7ccaac0fea7821d8fbeffb464
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-06-03 13:34:09 +0300

    .tt -> web-mode

--- rc/web.el
+++ rc/web.el
@@ -38,6 +38,7 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tt\\'" . web-mode))
 (add-to-list 'auto-mode-alist
              '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
 
