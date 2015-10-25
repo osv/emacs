4bc88572f9ba675fd759c9780d761c72bc993721
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-10-25 16:17:17 +0200

    jshint and npm support in compilation buffer

--- rc/js.el
+++ rc/js.el
@@ -88,3 +88,63 @@
           (lambda ()
             (make-local-variable 'js-indent-level)
             (setq js-indent-level 2)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; From https://github.com/startup-class/dotfiles/blob/master/.emacs.d/js-config.el
+
+;; Configure jshint for JS style checking.
+;;   - Install: $ npm install -g jshint
+;;   - Usage: Hit C-cC-u within any emacs buffer visiting a .js file
+(setq jshint-cli "jshint --show-non-errors ")
+(setq compilation-error-regexp-alist-alist
+      (cons '(jshint-cli "^\\([a-zA-Z\.0-9_/-]+\\): line \\([0-9]+\\), col \\([0-9]+\\)"
+                         1 ;; file
+                         2 ;; line
+                         3 ;; column
+                         )
+            compilation-error-regexp-alist-alist))
+(setq compilation-error-regexp-alist
+      (cons 'jshint-cli compilation-error-regexp-alist))
+
+(defun node-repl-comint-preoutput-filter (output)
+  "This function fixes the escape issue with node-repl in js-comint.el.
+  Heavily adapted from http://www.squidoo.com/emacs-comint (which
+  is in emacs/misc/comint_ticker)
+  Basically, by adding this preoutput filter to the
+  comint-preoutput-filter-functions list we take the output of
+  comint in a *js* buffer and do a find/replace to replace the
+  ANSI escape noise with a reasonable prompt.
+"
+  (if (equal (buffer-name) "*js*")
+      (progn
+        ;; Uncomment these to debug the IO of the node process
+        ;; (setq js-node-output output)
+        ;; (message (concat "\n----------\n" output "\n----------\n"))
+
+        ;; Replaced ^ with \^ to indicate that doesn't have to be
+        ;; at start of line
+        (replace-regexp-in-string
+         "\\\[0K" ""
+         (replace-regexp-in-string
+          "\\\[1G" ""
+          (replace-regexp-in-string
+           "\\\[0J" ""
+           (replace-regexp-in-string
+            "\\\[3G" ""
+            (replace-regexp-in-string
+             "\\\[0G" ""
+             (replace-regexp-in-string
+              "\\[2C" ""
+              (replace-regexp-in-string
+               "\\[0K" ""
+               (replace-regexp-in-string
+                "
+" "" output))))))))
+        )
+    output
+    )
+  )
+
+(add-hook 'comint-preoutput-filter-functions 'node-repl-comint-preoutput-filter)
+(add-hook 'comint-output-filter-functions 'node-repl-comint-preoutput-filter)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
