39792a7f98b93988522ea48d5c3f7e84468607ba
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2016-02-16 13:06:26 +0200

    alias ee end EDITOR

---
+++ init-tiny.el
@@ -0,0 +1,10 @@
+;; this version of emacs my setup used for `emacs -nw -q'
+(add-to-list 'load-path "~/emacs/external")
+
+(load "~/emacs/rc/core-packages.el")
+;; (load "~/emacs/rc/core-ui.el")
+;; (load "~/emacs/rc/core-lang.el")
+;; (load "~/emacs/rc/core-desktop.el")
+ (load "~/emacs/rc/company-completion.el")
+;; (load "~/emacs/rc/helm.el")
+ (load "~/emacs/pmade/emacs.d/pmade-colors.el")
